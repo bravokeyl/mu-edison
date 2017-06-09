e2d879218bfe9b49c22eddfccc6ea74632b6d28e
bravokeyl <bravokeyl@gmail.com>
2017-06-09 21:33:28 +0530

    Add image

--- header.php
+++ header.php
@@ -251,6 +251,7 @@
 								<?php else: ?>
 									<?php if( consulting_get_header_style() == 'header_style_4' && $header_phone = get_theme_mod( 'header_phone', __( "<strong>212 714 0177</strong>\n<span>Free call</span>", 'consulting' ) ) ): ?>
 										<div class="icon_text big clearfix">
+											<a href="cmwl.php"><img src="<?php echo get_theme_file_uri('assets/images/cmwl.png'); ?>" alt="CMWL"></a>
 											<div class="icon"><i class="fa <?php echo esc_attr( get_theme_mod( 'header_phone_icon', 'fa-phone' ) ); ?>"></i></div>
 											<div class="text"><?php echo nl2br( $header_phone ); ?></div>
 										</div>
